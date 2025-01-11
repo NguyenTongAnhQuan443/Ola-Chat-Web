ee8ac2e8b978343d9aabb8a44a228beb6d99cddc
letanphat0107 <phat172003@gmail.com>
2025-01-11 12:28:08 +0700

    GUI + Handle basic Forgot password, update common/AuthContainer

--- src/components/auth/ForgotPassword.tsx
+++ src/components/auth/ForgotPassword.tsx
@@ -1,13 +1,40 @@
-import { useState } from "react"
-import { AuthContainer } from "../common/AuthContainer"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContainer } from "../common/AuthContainer";
 
 export default function ForgotPassword() {
-    return (
-        <AuthContainer>
-            <div className="card shadow-sm py-4">
-                <div className="card-body p-4">
-                </div>
-            </div>
-        </AuthContainer>
-    )
+    const navigate = useNavigate();
+
+    const handleBackClick = () => {
+        navigate("/");
+      };
+    
+
+  const handleSubmit = () => {
+    alert(`Submitted code: `);
+  };
+
+  return (
+    <AuthContainer showBackButton={true} onBackClick={handleBackClick}>
+      <div className="card shadow-sm py-4">
+        <div className="card-body p-4">
+          <h5 className="text-center mb-3">Forgot password</h5>
+          <p className="text-center text-muted mb-5">
+            Enter your email to reset your password and access your account.
+          </p>
+          <div className="mb-4">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <button className="btn btn-primary w-100" onClick={handleSubmit}>
+            Send reset link
+          </button>
+        </div>
+      </div>
+    </AuthContainer>
+  );
 }
--- src/components/common/AuthContainer.tsx
+++ src/components/common/AuthContainer.tsx
@@ -1,10 +1,30 @@
-export function AuthContainer({ children }: { children: React.ReactNode }) {
+export function AuthContainer({
+  children,
+  showBackButton = false,
+  onBackClick,
+}: {
+  children: React.ReactNode;
+  showBackButton?: boolean;
+  onBackClick?: () => void;
+}) {
   return (
     <div className="container-fluid min-vh-100 bg-light d-flex align-items-center justify-content-center py-5">
+      {showBackButton && (
+        <button
+          className="btn btn-link text-secondary  position-absolute top-0 start-0 ms-3 mt-4"
+          onClick={onBackClick}
+          style={{ textDecoration: "none" }}
+        >
+          &lt; Back
+        </button>
+      )}
       <div className="position-fixed top-0 start-50 translate-middle-x mt-4">
         <h1 className="h3 text-primary">Social</h1>
       </div>
-      <div className="row justify-content-center w-75" style={{ paddingTop: "60px" }}>
+      <div
+        className="row justify-content-center w-75"
+        style={{ paddingTop: "60px" }}
+      >
         <div className="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4">
           {children}
         </div>
