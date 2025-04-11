5da3b36b3eecf70daefc757fe79dd0ab738d24e1
letanphat0107 <phat172003@gmail.com>
2025-04-12 04:09:30 +0700

    config axios,react quey, interface and api register

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from 'src/App'
+import './index.css'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
