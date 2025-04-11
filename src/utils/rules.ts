0d07e805a655625afa7bfc2c1cba53f1114d84a0
letanphat0107 <phat172003@gmail.com>
2025-04-12 03:02:25 +0700

    update rule

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -97,7 +97,7 @@ export const schema = yup.object({
     message: 'Giá không phù hợp',
     test: testPriceMinMax
   }),
-  name: yup.string().trim().required('Tên sản phẩm là bắt buộc')
+  name: yup.string().trim().required('Tên người dùng là bắt buộc')
 })
 
 export const userSchema = yup.object({
